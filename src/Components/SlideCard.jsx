b753c1d1bebfc31d0f959a8e454cfdb1c1ca33ad
Anurag Bansal <devanuragbansal@gmail.com>
2024-08-13 04:02:10 +0530

    minor changes

--- src/Components/SlideCard.jsx
+++ src/Components/SlideCard.jsx
@@ -11,14 +11,12 @@ function SlideCard() {
     }
 
   return (
-    <div className="">
-            <div className="relative w-[80%] sm:w-[25rem] h-[12rem] rounded bg-zinc-500 flex overflow-hidden">
-                <img src={img1} className={`w-full h-full object-cover shrink-0 ${val ? "-translate-x-[100%]" : "-translate-x-0"} transition-all duration-500`} />
-                <img src={img2} className={`w-full h-full object-cover shrink-0 ${val ? "-translate-x-[100%]" : "-translate-x-0"} transition-all duration-500`} />
-                <span className={`text-4xl bg-[#dadada7b] text-zinc-700 p-1 sm:p-2 rounded-full absolute z-10 bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer ${val ? "rotate-180" : "rotate-0"} transition-all duration-500`} onClick={handleClick}>
-                    <FaLongArrowAltRight />
-                </span>
-            </div>
+        <div className="relative w-[80%] sm:w-[25rem] h-[12rem] rounded bg-zinc-500 flex overflow-hidden">
+            <img src={img1} className={`w-full h-full object-cover shrink-0 ${val ? "-translate-x-[100%]" : "-translate-x-0"} transition-all duration-500`} />
+            <img src={img2} className={`w-full h-full object-cover shrink-0 ${val ? "-translate-x-[100%]" : "-translate-x-0"} transition-all duration-500`} />
+            <span className={`text-4xl bg-[#dadada7b] text-zinc-700 p-1 sm:p-2 rounded-full absolute z-10 bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer ${val ? "rotate-180" : "rotate-0"} transition-all duration-500`} onClick={handleClick}>
+                <FaLongArrowAltRight />
+            </span>
         </div>
   )
 }
